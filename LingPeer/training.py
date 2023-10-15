cfe9eefab0d56e1577b66c57fcf2c2d3564c1d72
cmunozperez <munozperezc@gmail.com>
2023-10-15 14:59:56 -0300

    Removed unnecessary imports

--- LingPeer/training.py
+++ LingPeer/training.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import joblib
 
